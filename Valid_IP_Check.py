481dc4a7c02f4a673ec7f2685b4968a29ed9ad6d
“Arin” <arinmaity3@gmail.com>
2023-11-18 14:12:18 +0530

    modified code for valid ip check

--- Valid_IP_Check.py
+++ Valid_IP_Check.py
@@ -1,16 +1,19 @@
 def valid_ip_check(i):
     try:
-        i_list = list(map(int, i.split(".")))
+        i_list = i.split(".")
     except ValueError as e:
         return print(e)
     if len(i_list) != 4:
         return print("This is an invalid IP address")
 
     for each in i_list:
-        if each > 256 or each < 0:
+        if len(each) > 3:
+            return print("This is an invalid IP address")
+        if int(each) > 256 or int(each) < 0:
             return print("This is an invalid IP address")
     return print("This is a valid IP address")
 
 
-user_input = input("Please enter the IP address(IPV4) :")
-valid_ip_check(user_input)
+# user_input = input("Please enter the IP address(IPV4) :")
+# valid_ip_check(user_input)
+valid_ip_check("100.00000.0.0")
